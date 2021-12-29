4eae6a811e5c55aac84dc98f361892ec7c8e0400
hiddenmario <96807162+hiddenmario@users.noreply.github.com>
2021-12-29 15:05:30 +0900

    Update Hello.java

--- GitTest01/src/hello/Hello.java
+++ GitTest01/src/hello/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello2");
+		System.out.println("Hello3");
+		System.out.println("Hello4");	
 	}
 }
